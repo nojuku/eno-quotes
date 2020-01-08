8a5d6c94667b946a754840c46a19ba659e50692e
Vladislav Luchnikov <nojuluseason@gmail.com>
2020-01-08 16:11:41 -0500

    removed duplicates, grouped functionality and all working with quote

--- helloworld.py
+++ helloworld.py
@@ -1,13 +1,25 @@
 import os
+import requests
+from lxml import html
 from flask import Flask
+from scraper import scrape
+
+def scrape():
+
+	page = requests.get('http://stoney.sb.org/eno/oblique.html')
+	tree = html.fromstring(page.content)
+	quote = tree.xpath('/html/body/center/h3/text()')
+
+	return str(quote[0])
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return scrape()
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
--- main.py
+++
@@ -1,9 +0,0 @@
-from lxml import html
-import requests
-
-page = requests.get('http://stoney.sb.org/eno/oblique.html')
-tree = html.fromstring(page.content)
-
-quote = tree.xpath('/html/body/center/h3/text()')
-
-print(quote)
